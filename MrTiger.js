a6c5844cb3c9d866582ceb676e7ee7d94e0ffcd8
TiktoKxmevan <106875792+TiktoKxmevan@users.noreply.github.com>
2022-06-14 20:33:19 +0300

    Update MrTiger.js

--- MrTiger.js
+++ MrTiger.js
@@ -22,10 +22,12 @@ client.on("ready", () => {
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     client.user.setActivity(STREAMING, {      
-    type:"Playing", 
+    type:"Playing pubg mobile", 
     url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 
-client.login("");
+client.login("0TI1 0 Tc10TcyOTMzNjYO
+Nzkw.GOt8BK.TSkneapY6Vw4gae
+nHyFAVJpVn67qylbwbyHUFI");
